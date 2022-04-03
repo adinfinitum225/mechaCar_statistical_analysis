e79b3cdc85d75256640e6afea00770716f38a3dd
Zach Wells <zach.13.wells@gmail.com>
2022-04-03 08:20:44 -0500

    Added suspension summaries to script

--- mechaCarChallenge.R
+++ mechaCarChallenge.R
@@ -7,3 +7,11 @@ head(mpg_table)
 lm(mpg ~ ., mpg_df)
 
 lm(mpg ~ ., mpg_df) %>% summary()
+
+
+suspension_df <- read.csv("data/Suspension_Coil.csv")
+
+total_summary <- summarize(suspension_df, Mean=mean(PSI), Median=median(PSI),
+                           Variance=var(PSI), SD=sd(PSI))
+lot_summary <- suspension_df %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
